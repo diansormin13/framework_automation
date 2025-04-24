d7c52901cb14d0f1af4f6792bd7416e4dda1b337
Dian Samuel Perisai Sormin <dian.samuel@adidata.co.id>
2025-04-24 17:44:25 +0700

    add feature

---
+++ Test Suites/02-Menu/12-Tugas Hari Ini/testtest.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>testtest</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>87923887-8f74-4b6b-b72e-336f31b49482</testSuiteGuid>
+   <testCaseLink>
+      <guid>9c4e00c7-d235-475d-80e8-8d6425dab74b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/02-Menu/12-Tugas Hari Ini/test_test</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
