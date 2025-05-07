26b70654b9ebf65965629fdd1b5cd9ba52da7644
samuel.adidata <dian.samuel@adidata.co.id>
2025-05-07 08:53:12 +0700

    refactor : change position db connection

--- Scripts/02-Menu/12-Tugas Hari Ini/SMTC_TugasHariIni_05-PMPViewDetailApprovalKlaimJHT_Success/Script1745238539747.groovy
+++ Scripts/02-Menu/12-Tugas Hari Ini/SMTC_TugasHariIni_05-PMPViewDetailApprovalKlaimJHT_Success/Script1745238539747.groovy
@@ -21,11 +21,6 @@ import com.kms.katalon.core.util.KeywordUtil as KeywordUtil
 
 KeywordUtil.logInfo('TC-TDL-006 - Sebagai PMP, saya berhasil melihat detail approval Klaim JHT pada section role yang aktif')
 
-
-def categoryId = CustomKeywords.'utility.ConfigYuga.getDatafromDB'(GlobalVariable.configDB, categoryID)
-
-def subCategoryID = CustomKeywords.'utility.ConfigYuga.getDatafromDB'(GlobalVariable.configDB, subCategoryId)
-
 CustomKeywords.'sectionMenu.utilityMenu.changeRoleSMILE'()
 
 // Call the test case for selecting role in SMILE
@@ -42,6 +37,10 @@ def screenshoot = new utilityMenu()
 CustomKeywords.'sectionMenu.utilityMenu.clickTab'(param)
 
 CustomKeywords.'sectionMenu.todolist.viewPelayananCabang'()
+
+def categoryId = CustomKeywords.'utility.ConfigYuga.getDatafromDB'(GlobalVariable.configDB, categoryID)
+
+def subCategoryID = CustomKeywords.'utility.ConfigYuga.getDatafromDB'(GlobalVariable.configDB, subCategoryId)
 
 screenshoot.takeScreenshot('beforedetailRoleActivePMPKlaimJHT')
 
