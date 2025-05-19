4772a6b02df182d755a22dbe532a3ad21476b205
samuel.adidata <dian.samuel@adidata.co.id>
2025-05-19 14:38:54 +0700

    refactor: delete change role from tc-12

--- Scripts/02-Menu/12-Tugas Hari Ini/SMTC_TugasHariIni_11-PMPViewPelayananKanal_Success/Script1745293048712.groovy
+++ Scripts/02-Menu/12-Tugas Hari Ini/SMTC_TugasHariIni_11-PMPViewPelayananKanal_Success/Script1745293048712.groovy
@@ -20,7 +20,7 @@ import com.kms.katalon.core.util.KeywordUtil
 
 KeywordUtil.logInfo("TC-TDL-012 - Sebagai Role PMP/KAKACAB, Saya dapat melihat tab Lihat Semua, Pelayanan Cabang, dan Pelayanan Kanal")
 
-CustomKeywords.'sectionMenu.utilityMenu.changeRoleSMILE'()
+// CustomKeywords.'sectionMenu.utilityMenu.changeRoleSMILE'()
 // Call the test case for selecting role in SMILE
 WebUI.callTestCase(
     findTestCase('Test Cases/01-Login/Login-pilihRoleSSMILE-02_Success'),
