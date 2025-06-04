622c04dae8ad2343e11d1c532596e7e3edbf341c
samuel.adidata <dian.samuel@adidata.co.id>
2025-06-04 08:44:46 +0700

    refactor:delete file

--- Test Suites/03-Collection/01-Login/SMTSC_Login_01-userAuthentication.ts
+++ Test Suites/03-Collection/01-Login/SMTSC_Login_01-userAuthentication.ts
@@ -12,7 +12,7 @@
             <groupName>Web Desktop</groupName>
             <profileName>user-authentication</profileName>
             <requireConfigurationData>false</requireConfigurationData>
-            <runConfigurationId>Chrome</runConfigurationId>
+            <runConfigurationId>Chrome (headless)</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/01-Login/SMTS_Login_01-Aktivasi2FA</testSuiteEntity>
