ac8a2d84142675bb380717e566f4b78861acb82c
habibipti <muhammad.rifqi@bpjsketenagakerjaan.go.id>
2025-06-26 15:41:08 +0700

    Revert "KN9006 - Laporan Perusahaan Binaan"
    This reverts commit c9abeca5c5ac3adafb6be812ce7a8e58e3a292d4.

--- Keywords/sectionLogin/Overtime.groovy
+++ Keywords/sectionLogin/Overtime.groovy
@@ -19,7 +19,7 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 
-
+import CustomKeywords
 import internal.GlobalVariable
 
 import javax.mail.*
